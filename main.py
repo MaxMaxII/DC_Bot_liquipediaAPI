379cf6f7652f9403a71172833573ea228ce1dbf1
Max Schuessler <max.schuessler@student.tugraz.at>
2021-05-24 18:26:27 +0200

    update main

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ import discord as dc
 TOKEN = 'Token in here pls'
 
 description = '''A bot which's existence has a purpouse and one purpose only, tell you when harstem playes, and where to see it.'''
+intents = dc.Intents.default() #things the bot can do/ events that can be listend to
 
-intents = dc.Intents.default()
 
-
-bot = dc.commands.Bot(command_prefix='?', description=description, intents=intents)
-
-@bot.command()
-async def next(channel):
-    pass
+#bot = dc.commands.Bot(command_prefix='?', description=description, intents=intents)
+#@bot.command()
+#async def next(channel):
+#    pass
 
 class HarstemBot(dc.Client):
     async def on_ready(self):
